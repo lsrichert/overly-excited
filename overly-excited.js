21a5d2177a312e62cb1e3925a69088da28e9127e
Lauren Richert <lsrichert@gmail.com>
2018-05-25 10:11:23 -0500

    added example from kyle

--- overly-excited.js
+++ overly-excited.js
@@ -22,3 +22,34 @@ console.log(buildMeUp += " " + theWordArray [i])
 
 // Invoke the function and pass in the array
 addExcitement(sentence)
+
+
+// Excitement Exercise
+
+const sentence = ['This', 'is', 'so', 'fun', 'learning', 'scope'];
+
+const printWords = () => {
+    let printWords = '';
+    for (let i = 0; i < sentence.length; i++) {
+        printWords += sentence[i] + ' ';
+        console.log(printWords)
+    }
+}
+
+// printWords()
+
+const addSymbol = () => {
+    let printWords = '';
+
+    for (let i = 0; i < sentence.length; i++) {
+        let count = i + 1;
+        if (count % 3 === 0) {
+            printWords += sentence[i] + '! '
+        }
+        else {
+            printWords += sentence[i] + ' '
+        }
+        console.log(printWords)
+    }
+}
+// addSymbol()
